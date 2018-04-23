e2585d22dcbf64601f04cb0e668c574970cb5d3c
MCMrARM <mcmrarm@gmail.com>
2018-04-23 20:53:43 +0200

    Page loaded callback

--- extension/main.cpp
+++ extension/main.cpp
@@ -2,9 +2,15 @@
 #include <JavaScriptCore/JavaScript.h>
 #include "ExternalObject.h"
 
-static JSValueRef
-property_func(JSContextRef context, JSObjectRef function, JSObjectRef thisObject, size_t argumentCount,
-              const JSValueRef arguments[], JSValueRef* exception) {
+
+static void
+page_loaded_callback(WebKitWebPage* web_page, gpointer user_data) {
+    printf("Page loaded\n");
+}
+
+static void
+page_created_callback(WebKitWebExtension* extension, WebKitWebPage* web_page, gpointer user_data) {
+    g_signal_connect(web_page, "document-loaded", G_CALLBACK(page_loaded_callback), NULL);
 }
 
 
@@ -17,18 +23,14 @@ window_object_cleared_callback(WebKitScriptWorld* world, WebKitWebPage* web_page
 
     JSObjectRef external = JSObjectMake(ctx, ExternalObject::get_class_def(), nullptr);
     JSObjectSetProperty(ctx, global, JSStringCreateWithUTF8CString("external"), external, kJSPropertyAttributeNone, nullptr);
-
-/*
-    JSObject external = JSObject();
-    JSObjectSetProperty(ctx, global, JSString("Property"),
-                        JSObject::MakeFunctionWithCallback(ctx, JSString("Property"), property_func),
-                        kJSPropertyAttributeDontDelete | kJSPropertyAttributeReadOnly, nullptr);*/
 }
 
 
 extern "C" G_MODULE_EXPORT void
 webkit_web_extension_initialize(WebKitWebExtension* extension) {
     g_print("webkit_web_extension_initialize\n");
+    g_signal_connect (extension, "page-created",
+                      G_CALLBACK(page_created_callback), NULL);
     g_signal_connect (webkit_script_world_get_default(), "window-object-cleared",
                       G_CALLBACK(window_object_cleared_callback), NULL);
 }
